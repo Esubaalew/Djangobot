84a2057e6d4bcbbba4c65c7ed6fcc7cfbfa60ce2
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-10-01 15:13:57 +0300

    Create bot.py

---
+++ bott/bot.py
@@ -0,0 +1,35 @@
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+)
+from os import getenv
+import logging
+from telegram import Update
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Start command to show categories."""
+    await update.message.reply_text("Hey Welcome!!")
+
+
+async def bot_tele(text):
+    # Create application
+    application = (
+        Application.builder().token("7421427110:AAF0PzXYiahZYsV9ioYDMgKUFZqHyPWekvk").build()
+    )
+
+    
+    # Register handlers
+    application.add_handler(CommandHandler("start", start))
+
+    # Start application
+    await application.bot.set_webhook(url='https://testing.4gmobiles.com/')
+    await application.update_queue.put(
+        Update.de_json(data=text, bot=application.bot)
+    )
+    async with application:
+        await application.start()
+        await application.stop()
